e36b2085fd6d047a6ba5ecd1f12b1f771d058377
bluthej <joffrey.bluthe@e.email>
2023-11-04 17:16:51 +0100

    Add `cat-file` subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
+use flate2::bufread::ZlibDecoder;
 use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::PathBuf;
 
 use clap::Parser;
 use clap::Subcommand;
@@ -14,6 +19,10 @@ struct Cli {
 #[derive(Subcommand)]
 enum Command {
     Init,
+    CatFile {
+        #[arg(short = 'p')]
+        blob_sha: Option<String>,
+    },
 }
 
 fn main() -> Result<()> {
@@ -21,6 +30,10 @@ fn main() -> Result<()> {
 
     match &cli.command {
         Command::Init => git_init(),
+        Command::CatFile { blob_sha } => match blob_sha {
+            Some(blob_sha) => git_cat_file(blob_sha),
+            None => Ok(()),
+        },
     }
 }
 
@@ -30,5 +43,24 @@ fn git_init() -> Result<()> {
     fs::create_dir(".git/refs")?;
     fs::write(".git/HEAD", "ref: refs/heads/master\n")?;
     println!("Initialized git directory");
+    Ok(())
+}
+
+fn git_cat_file(blob_sha: &str) -> Result<()> {
+    let path = PathBuf::from(format!(
+        ".git/objects/{}/{}",
+        &blob_sha[..2],
+        &blob_sha[2..]
+    ));
+
+    let f = File::open(path)?;
+    let reader = BufReader::new(f);
+
+    let mut z = ZlibDecoder::new(reader);
+    let mut s = String::new();
+    z.read_to_string(&mut s)?;
+
+    print!("{s}");
+
     Ok(())
 }
